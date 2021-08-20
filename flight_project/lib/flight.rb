219ef6263fc05818c2675be39d89654f8abb82dd
Tristan Angeles <setswipe@gmail.com>
2021-08-20 14:21:32 -0400

    flight (completed)

--- flight_project/lib/flight.rb
+++ flight_project/lib/flight.rb
@@ -1,6 +1,40 @@
 require "byebug"
 class Flight
 
+  attr_reader :passengers 
+  
+  def initialize(flight_num, capacity)
+    @flight_number = flight_num
+    @capacity = capacity
+    @passengers = []
+  end
 
+  def full?
+    if @passengers.length == @capacity
+      return true
+    end
+
+    false
+  end
+
+  def board_passenger(passenger)
+    if !full? && passenger.has_flight?(@flight_number)
+      @passengers << passenger
+    end
+  end
+
+  def list_passengers
+    arr = []
+    @passengers.each {|ele| arr<<ele.name}
+    arr
+  end
+
+  def [] (idx)
+    @passengers[idx]
+  end
+
+  def << (passenger)
+    board_passenger(passenger)
+  end
 
 end # end Flight Class
